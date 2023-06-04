429513a8e150570b7bc8c5af2181045441f0c7c6
Greg Lewin <gcl8a@icloud.com>
2023-06-03 20:38:32 -0400

    untested example code

--- examples/IRremote/src/IRremote.cpp
+++ examples/IRremote/src/IRremote.cpp
@@ -1,11 +1,14 @@
 #include <IRdecoder.h>
 
+IRDecoder decoder (0);
+
 void setup(void)
 {
-
+    decoder.init();
 }
 
 void loop(void)
 {
-    
+    int16_t keyCode = decoder.getKeyCode();
+    if(keyCode != -1) Serial.println(keyCode);
 }
